95ec56bdadbf8dae33dda39d7d25cc7daad8f2fd
joyahmed <joy_ahmed_007@yahoo.com>
2024-11-02 21:02:04 +0600

    Init

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import {
+	Image,
+	Platform,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
+
+export default function HomeScreen() {
+	return (
+		<View style={styles.container}>
+			<Text>Hello World</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		flex: 1,
+		justifyContent: 'center'
+	}
+});
